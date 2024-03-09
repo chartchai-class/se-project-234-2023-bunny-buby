8d84bbf2b84fedf7439f8841d47dbbfeab6cc1fa
mpani03 <milkmilkxciii@gmail.com>
2024-03-09 22:22:36 +0700

    create header

--- app.js
+++ app.js
@@ -2,11 +2,6 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.set('view engine', 'ejs');
-
-app.get('/', (req, res) => {
-    res.render("pages/index")
-})
 
 app.listen(port, () => {
     console.log(`App listening at port ${port}`)
