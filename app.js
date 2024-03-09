a662595bc6c1620c15d735883a2ecf4e53adf249
thanyasiriisnotavailable <thanyasiri_ch@cmu.ac.th>
2024-03-09 15:16:17 +0700

    change structure to use ejs instead and installed nodemon

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.render("pages/index")
+    res.render('pages/index')
 })
 
 app.listen(port, () => {
