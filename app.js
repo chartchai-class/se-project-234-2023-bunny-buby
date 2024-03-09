41003418940b12231c6440fea59a0569bffe3ac7
Sorod Yawa <sorod_yawa@cmu.ac.th>
2024-03-09 22:55:14 +0700

    created header and navbar

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+
+app.get("/", (req, res) => {
+    res.render("webStore/login");
+})
+
 
 app.listen(port, () => {
     console.log(`App listening at port ${port}`)
