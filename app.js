d711e3de6e3145051cc597a301fe3c00155ae905
Sorod Yawa <sorod_yawa@cmu.ac.th>
2024-03-14 05:22:09 +0700

    Home page - imcomplete

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
 app.get("/", (req, res) => {
-    res.render("WebStore/login");
+    res.render("webStore/home");
 })
 
 app.get("/signup", (req, res) => {
