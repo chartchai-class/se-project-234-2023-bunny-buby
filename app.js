4ae953d3921bcfbafe9d46b9485ddc88b5b4866a
mpani03 <milkmilkxciii@gmail.com>
2024-03-16 16:57:51 +0700

    category templates are now linked to each category

--- app.js
+++ app.js
@@ -9,17 +9,42 @@ app.get("/", (req, res) => {
     res.render("WebStore/home");
 })
 
-app.get("/WebStore/contact", (req, res) => {
-    res.render("WebStore/contact");
+app.get("/WebStore/home", (req, res) => {
+    res.render("WebStore/home");
+});
+
+app.get("/WebStore/login", (req, res) => {
+    res.render("WebStore/login");
 });
 
 app.get("/signup", (req, res) => {
     res.render("WebStore/signup");
 });
 
-app.get("/WebStore/home", (req, res) => {
-    res.render("WebStore/home");
+app.get("/WebStore/contact", (req, res) => {
+    res.render("WebStore/contact");
 });
+
+app.get("/WebStore/Category/allproduct", (req, res) => {
+    res.render("WebStore/Category/allproduct");
+});
+
+app.get("/WebStore/Category/men", (req, res) => {
+    res.render("WebStore/Category/men");
+});
+
+app.get("/WebStore/Category/women", (req, res) => {
+    res.render("WebStore/Category/women");
+});
+
+app.get("/WebStore/Category/kids", (req, res) => {
+    res.render("WebStore/Category/kids");
+});
+
+app.get("/WebStore/Category/accessories", (req, res) => {
+    res.render("WebStore/Category/accessories");
+});
+
 app.get("/back-office/welcome", (req, res) => {
     res.render("back-office/welcome");
 });
